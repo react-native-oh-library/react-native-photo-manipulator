99c5d57e2038d3d08d05f9351020cdae50086e92
李琦 <liqi219@h-partners.com>
2025-09-28 16:19:10 +0800

    chore:删除源库的代码，供后续鸿蒙代码适配提交
    Signed-off-by: 李琦 <liqi219@h-partners.com>

--- example/app/example/settings.ts
+++
@@ -1,4 +0,0 @@
-export const IMAGE =
-  'https://github.com/guhungry/react-native-photo-manipulator/raw/master/docs/demo-background.jpg';
-export const OVERLAY =
-  'https://github.com/guhungry/react-native-photo-manipulator/raw/master/docs/demo-icon.png';
--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  PhotoManipulatorExample
-//
-
-import Foundation
